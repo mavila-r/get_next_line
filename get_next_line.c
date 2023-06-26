3b39fd5f01b64ce87d2487a9b811f8987e0f4606
Tere Ávila <131455847+mavila-r@users.noreply.github.com>
2023-06-26 15:45:33 +0200

    Mejora puerca 1

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mavila-r <mavila-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 16:03:12 by mavila-r          #+#    #+#             */
-/*   Updated: 2023/06/13 16:51:19 by mavila-r         ###   ########.fr       */
+/*   Updated: 2023/06/14 13:15:13 by mavila-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,30 @@
 
 char *get_next_line(int fd)
 {
-char	*line;
+char		*line;
+int			book;
+static int	i;
 
-line = malloc(/*Tamaño memoria*/);
+line = malloc(/*Tamaño memoria que no sé como saco*/);
 if (line == NULL)
 	return (NULL);
-read(fd, line, /*Tamaño memoria*/);
+book = read(fd, line, /*Tamaño memoria que no sé como saco*/);
+if (book == -1)
+	return (NULL);
+while (file != '\0')
+	{
+		if (file != '\n')
+		{
+			line = file[i];
+			i++;
+		}
+		else
+			return (line);
+	}
+}
 
+int main (void)
+{
+	
+	return (0);
 }
