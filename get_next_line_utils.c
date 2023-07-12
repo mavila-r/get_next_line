96c3debb05f3ecbc97e86549ee8b477209c6e3a9
Tere Ávila <131455847+mavila-r@users.noreply.github.com>
2023-07-12 10:51:08 +0200

    Update 12 Jul

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,3 +10,71 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+char *ft_strjoin(char const *s1, char const *s2)
+{
+  char  *res_str;
+  size_t  len;
+  int  n1;
+  int  n2;
+
+  i = 0;
+  j = 0;
+  len = ((size_t)ft_strlen(s1)+(size_t)ft_strlen(s2));
+  res_str = ((char *)malloc((len + 1)*sizeof(char)));
+  if (!res_str)
+    return (NULL);
+  while (s1[n1])
+    {
+      res_str[n1] = s1[n1];
+      n1++;
+    }
+  while (s2[n2])
+    {
+      res_str[n1 + n2] = s1[n2];
+      n2++;
+    }
+  res_str[n1 + n2] = '\0';
+  return (res_str);
+}
+
+void *ft_calloc(size_t num, size_t size)
+{
+  char  *ptr;
+  size_t  i;
+  ptr = (char *)malloc(size*num);
+  if (!ptr)
+      return (0);
+  i = 0;
+  while (i < (num * size)
+    {
+    ptr[i];
+    i++;
+    }
+return (ptr);
+}
+
+int ft_strlen(const char *str)
+{
+  int  count;
+  count = 0;
+  while (str[count] != '\0')
+      count++;
+  return (count);
+}
+
+int ft_strchr(char *s, int c)
+{
+    int  i;
+
+    i = 0;
+    if (!s)
+        return (0);
+    while (s[i])
+        {
+            if (s[i] == (char)c)
+                return (1);
+            i++;
+        }
+    return (0);
+}
+
